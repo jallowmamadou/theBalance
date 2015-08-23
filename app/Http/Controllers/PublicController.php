0c3e56a024196b4113f4bcfa066381e05417e23c
mamadou jallow <sulsiralumo@gmail.com>
2015-08-23 22:35:57 +0000

    first draft

---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by mamadou.
+ * User: mamadou
+ * Date: 8/23/2015
+ * Time: 8:56 PM
+ */
+
+namespace app\Http\Controllers;
+
+use App\Http\Controllers\Controller as Controller;
+
+class PublicController extends Controller{
+
+    public function showPublic(){
+        return "hey there";
+    }
+
+
+    public function showPublic(){
+        return "hey there";
+    }
+
+
+    public function showPublic(){
+        return "hey there";
+    }
+
+    
+} 
